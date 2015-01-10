4fea4f19dafc8788bf167053389cf6fd556fa10f
Yi <y22yin@uwaterloo.ca>
2015-01-10 06:48:00 -0500

    initial commit real-time coordinate update

---
+++ app/src/main/java/com/example/yi/binger/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.yi.binger;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import org.apache.http.client.methods.HttpPut;
+
+//import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+//import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+
+
+
+public class MainActivity extends ActionBarActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mLastLocation;
+
+    //logical - move to server side asap
+    private int RADIUS_RANGE = 10;
+
+    //UI variables
+    private TextView myLong, myLat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(15000);
+        mLocationRequest.setFastestInterval(10000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        setContentView(R.layout.activity_main);
+
+        //install UI fields
+        myLong = (TextView)findViewById(R.id.my_long);
+        myLat = (TextView)findViewById(R.id.my_lat);
+        Log.i("mainActivity", "onCreate");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("mainActivity", "onStart");
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i("mainActivity", "onConnected");
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            myLong.setText(String.valueOf(mLastLocation.getLatitude()));
+            myLat.setText(String.valueOf(mLastLocation.getLongitude()));
+        } else {
+            //error msg
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("mainActivity", "onConnectionSuspended");
+    }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("mainActivity", "onConnectionFailed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i("mainActivity", "onConnectionFailed");
+        if (location != null) {
+            mLastLocation = location;
+            myLong.setText(String.valueOf(mLastLocation.getLatitude()));
+            myLat.setText(String.valueOf(mLastLocation.getLongitude()));
+
+            //send {uid, long, lat} to server
+            //http://stackoverflow.com/questions/10432885/http-put-request
+            HttpPut httpPut = new HttpPut("http://server/updateLocation.php");
+
+        } else {
+            //error msg
+        }
+
+
+    }
+}
